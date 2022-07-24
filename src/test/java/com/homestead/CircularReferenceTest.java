e5d8cbf61ec8ce60035629c3e39d3e17a586fd49
yanghanbin <123456@qq.com>
2022-07-24 22:35:56 +0800

    left join circular reference

---
+++ src/test/java/com/homestead/CircularReferenceTest.java
@@ -0,0 +1,32 @@
+package com.homestead;
+
+import com.homestead.entity.Blog;
+import com.homestead.mapper.UserMapper;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author HanBin_Yang
+ * @since 2022/7/24 22:26
+ */
+public class CircularReferenceTest {
+    private SqlSessionFactory factory;
+
+    @Before
+    public void init() {
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        factory = builder.build(FirstCacheTest.class.getResourceAsStream("/mybatis-config.xml"));
+    }
+
+
+    @Test
+    public void leftJoinCircularTest() {
+        SqlSession sqlSession = factory.openSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        Blog blog = mapper.selectByBlogIdCollectionCircu(10);
+        System.out.println("end");
+    }
+}
